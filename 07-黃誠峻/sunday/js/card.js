aadcce475837eee2cac64d31e9cc302519ff038b
CJ0423 <b09785157888@gmail.com>
2022-12-10 18:22:34 +0800

    在網咖打程式 我真棒(?

--- 07-黃誠峻/sunday/js/card.js
+++ 07-黃誠峻/sunday/js/card.js
@@ -1,10 +1,10 @@
 var s=document.querySelector("#taipei")
 var cleanArea=[];
-// console.log(s)
+// //console.log(s)
 // // dataset 用來取得放在上面並且開頭是data的
-// console.log(s.dataset.name)
+// //console.log(s.dataset.name)
 s.addEventListener("click",function(){
-// console.log("台北")
+// //console.log("台北")
 })
 // 產生事件監聽器
 const allArea = document.querySelector("#allArea")
@@ -13,70 +13,92 @@ const centerArea = document.querySelector("#centerArea")
 const southArea = document.querySelector("#southArea")
 const eastArea = document.querySelector("#eastArea")
 const island = document.querySelector("#island")
+const am_am=document.querySelector("#am_am")
+const am_pm=document.querySelector("#am_pm")
+const pm_am=document.querySelector("#pm_am")
 
+var j=1;
 
+am_am.addEventListener("click",function(){
+allCards0.style="display:"
+allCards1.style="display:none"
+allCards2.style="display:none"
+return j=0
+})
+am_pm.addEventListener("click",function(){
+allCards0.style="display:none"
+allCards1.style="display:"
+allCards2.style="display:none"
+return j=1
+})
+pm_am.addEventListener("click",function(){
+allCards0.style="display:none"
+allCards1.style="display:none"
+allCards2.style="display:"
+return j=2
+})
 
 allArea.addEventListener("click",function(){
     for(i=0;i<=21;i++){
         
-     document.querySelector(`#cleanArea${i}`).style.display=""
+     document.querySelector(`#cleanArea${j}${i}`).style.display=""
     // document.querySelector(`#cleanArea${i}`)
     }
     })
 northArea.addEventListener("click",function(){
     for(i=0;i<=21;i++){
         if(i<=5){
-            document.querySelector(`#cleanArea${i}`).style.display=""
+            document.querySelector(`#cleanArea${j}${i}`).style.display=""
         }
-        else{document.querySelector(`#cleanArea${i}`).style.display="none"}
+        else{document.querySelector(`#cleanArea${j}${i}`).style.display="none"}
     // document.querySelector(`#cleanArea${i}`)
     }
     })
 centerArea.addEventListener("click",function(){
     for(i=0;i<=21;i++){
         if(i>=6&&i<=9){
-            document.querySelector(`#cleanArea${i}`).style.display=""
+            document.querySelector(`#cleanArea${j}${i}`).style.display=""
         }
-        else{document.querySelector(`#cleanArea${i}`).style.display="none"}
+        else{document.querySelector(`#cleanArea${j}${i}`).style.display="none"}
     }
 })
 southArea.addEventListener("click",function(){
     for(i=0;i<=21;i++){
         if(i>=10&&i<=15){
-            document.querySelector(`#cleanArea${i}`).style.display=""
+            document.querySelector(`#cleanArea${j}${i}`).style.display=""
         }
-        else{document.querySelector(`#cleanArea${i}`).style.display="none"}
+        else{document.querySelector(`#cleanArea${j}${i}`).style.display="none"}
     }
     })
 eastArea.addEventListener("click",function(){
     for(i=0;i<=21;i++){
         if(i>=16&&i<=18){
-            document.querySelector(`#cleanArea${i}`).style.display=""
+            document.querySelector(`#cleanArea${j}${i}`).style.display=""
         }
-        else{document.querySelector(`#cleanArea${i}`).style.display="none"}
+        else{document.querySelector(`#cleanArea${j}${i}`).style.display="none"}
     }
     })
 island.addEventListener("click",function(){
     for(i=0;i<=21;i++){
         if(i>=19&&i<=21){
-            document.querySelector(`#cleanArea${i}`).style.display=""
+            document.querySelector(`#cleanArea${j}${i}`).style.display=""
         }
-        else{document.querySelector(`#cleanArea${i}`).style.display="none"}
+        else{document.querySelector(`#cleanArea${j}${i}`).style.display="none"}
     }
     })
 
 
 // centerArea.addEventListener("click",function(){
-//     console.log(this);
+//     //console.log(this);
 //     })
 // southArea.addEventListener("click",function(){
-//     console.log(this);
+//     //console.log(this);
 //     })
 // eastArea.addEventListener("click",function(){
-//     console.log(this);
+//     //console.log(this);
 //     })
 // island.addEventListener("click",function(){
-//     console.log(this);
+//     //console.log(this);
 //     })
 
 
@@ -92,10 +114,12 @@ const locationNum=[1,3,4,5,13,18,8,11,14,20,0,2,6,9,15,17,7,10,12,16,19,21]
 // const islandArray=[16,19,21]
 
 
-// console.log(locationArray[21])//確定位置
+// //console.log(locationArray[21])//確定位置
 
 
-let allCards=document.querySelector("#allCards")
+let allCards0=document.querySelector("#allCards0")
+let allCards1=document.querySelector("#allCards1")
+let allCards2=document.querySelector("#allCards2")
 
 // 與api連線
 fetch( "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-AA300EC1-31BA-465E-B669-6CA2C320A195")
@@ -105,27 +129,29 @@ return response.json();
 .then(function(myJson) {
 
 // // 以下為如何抓取資料的方法 接著我們嘗試去取得各種我們所需要的資料並且加以定義吧!
-console.log(myJson)//取得大致資料
-// console.log(myJson.records.location[11]) //取得地點
-// console.log(myJson.records.location[11].weatherElement[0])//取得天氣現象(wx)
-// console.log(myJson.records.location[11].weatherElement[0].time[0])//進入到早上六點到晚上六點時段
-// console.log(myJson.records.location[11].weatherElement[0].time[0].parameter)//進入到選擇天氣狀況或是天氣狀況代碼
-// console.log(myJson.records.location[11].weatherElement[0].time[0].parameter.parameterName)
+//console.log(myJson)//取得大致資料
+// //console.log(myJson.records.location[11]) //取得地點
+// //console.log(myJson.records.location[11].weatherElement[0])//取得天氣現象(wx)
+// //console.log(myJson.records.location[11].weatherElement[0].time[j])//進入到早上六點到晚上六點時段
+// //console.log(myJson.records.location[11].weatherElement[0].time[j].parameter)//進入到選擇天氣狀況或是天氣狀況代碼
+// //console.log(myJson.records.location[11].weatherElement[0].time[j].parameter.parameterName)
 //選擇天氣狀況名字
-var startTime=myJson.records.location[11].weatherElement[0].time[0].startTime//取得開始時間
-var endTime=myJson.records.location[11].weatherElement[0].time[0].endTime//取得結束時間
+var startTime=myJson.records.location[11].weatherElement[0].time[1].startTime//取得開始時間
+var endTime=myJson.records.location[11].weatherElement[0].time[1].endTime//取得結束時間
 
 // var area=
-// console.log("庫"+myJson.records.location[12].weatherElement[3].time[0].parameter.parameterName)//取得舒適度
+// //console.log("庫"+myJson.records.location[12].weatherElement[3].time[j].parameter.parameterName)//取得舒適度
 // 產生卡片
-allCards.innerHTML=""
-    for(let i=0;i<=21;i++){
+allCards2.innerHTML=""
+   for(let j=0;j<=2;j++)
+   { for(let i=0;i<=21;i++){
 var city=myJson.records.location[locationNum[i]].locationName; //取得縣市名字
-console.log(city)
-var weather=myJson.records.location[locationNum[i]].weatherElement[0].time[0].parameter.parameterName;
-console.log(weather)  //取得天氣狀況
-var weatherValue=myJson.records.location[locationNum[i]].weatherElement[0].time[0].parameter.parameterValue//取得天氣狀況數值
-
+//console.log(city)
+var weather=myJson.records.location[locationNum[i]].weatherElement[0].time[j].parameter.parameterName;
+//console.log(weather)  //取得天氣狀況
+var weatherValue=myJson.records.location[locationNum[i]].weatherElement[0].time[j].parameter.parameterValue//取得天氣狀況數值
+var startTime=myJson.records.location[11].weatherElement[0].time[j].startTime//取得開始時間
+var endTime=myJson.records.location[11].weatherElement[0].time[j].endTime//取得結束時間
 if(weatherValue<=3){
     var img="cloud"
 }
@@ -137,21 +163,34 @@ else{
 }
 
 
-var lowTempture=myJson.records.location[locationNum[i]].weatherElement[2].time[0].parameter.parameterName //取得最低溫度
+var lowTempture=myJson.records.location[locationNum[i]].weatherElement[2].time[j].parameter.parameterName //取得最低溫度
 
-console.log(lowTempture)
+//console.log(lowTempture)
 
-var highTempture=myJson.records.location[locationNum[i]].weatherElement[4].time[0].parameter.parameterName//取得最高溫度
-console.log(highTempture)
+var highTempture=myJson.records.location[locationNum[i]].weatherElement[4].time[j].parameter.parameterName//取得最高溫度
+//console.log(highTempture)
  
 
-var chance=`降雨機率:${myJson.records.location[locationNum[i]].weatherElement[1].time[0].parameter.parameterName}%`//降雨機率
+var chance=`降雨機率:${myJson.records.location[locationNum[i]].weatherElement[1].time[j].parameter.parameterName}%`//降雨機率
 
-var comfort=myJson.records.location[locationNum[i]].weatherElement[3].time[0].parameter.parameterName
+var comfort=myJson.records.location[locationNum[i]].weatherElement[3].time[j].parameter.parameterName
+if(j==0){
+    allCards0.innerHTML+=`<b><div id="cleanArea${j}${i}" class="card"> <div class="rain"><img src="../images/${img}.svg" alt=""></div><div class="city">${city}</div><div class="normal">溫度:${lowTempture}°~${highTempture}°:</div><div class="normal">${chance}</div><div class="normal">天氣:${weather}</div><div class="normal">${comfort}</div></div></div><b>`
+    allCards0.style="display:none"
 
-allCards.innerHTML+=`<b><div id="cleanArea${i}" class="card"> <div class="rain"><img src="../images/${img}.svg" alt=""></div><div class="city">${city}</div><div class="normal">溫度:${lowTempture}°~${highTempture}°:</div><div class="normal">${chance}</div><div class="normal">天氣:${weather}</div><div class="normal">${comfort}</div></div></div><b>`
+}
+else if(j==1){
+allCards1.innerHTML+=`<b><div id="cleanArea${j}${i}" class="card"> <div class="rain"><img src="../images/${img}.svg" alt=""></div><div class="city">${city}</div><div class="normal">溫度:${lowTempture}°~${highTempture}°:</div><div class="normal">${chance}</div><div class="normal">天氣:${weather}</div><div class="normal">${comfort}</div></div></div><b>`
+}
 
-     }
+else if(j==2){
+    allCards2.innerHTML+=`<b><div id="cleanArea${j}${i}" class="card"> <div class="rain"><img src="../images/${img}.svg" alt=""></div><div class="city">${city}</div><div class="normal">溫度:${lowTempture}°~${highTempture}°:</div><div class="normal">${chance}</div><div class="normal">天氣:${weather}</div><div class="normal">${comfort}</div></div></div><b>`
+    allCards2.style="display:none"
+
+    console.log(document.getElementById(`cleanArea${j}${i}`))
+   
+}
+     }}
     }
 );
 
