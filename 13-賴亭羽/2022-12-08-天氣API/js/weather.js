7e6595aca0516fef5ecda5fec229e8122ad2a386
LAI032 <118946881+LAI032@users.noreply.github.com>
2022-12-12 16:37:58 +0800

    2022/12/12

--- 13-賴亭羽/2022-12-08-天氣API/js/weather.js
+++ 13-賴亭羽/2022-12-08-天氣API/js/weather.js
@@ -1,7 +1,8 @@
-//送出請求回傳資料，得到promise，所以等他回應之後執行...
 const content = document.querySelector('.container');
+const picture=document.querySelector('.picture');
 fetch("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-AA300EC1-31BA-465E-B669-6CA2C320A195")
   .then(function (response) {
+    //送出請求回傳資料，得到promise，所以等他回應之後執行...
     //但回應之後回傳的資料太多，需要過濾，運用json()，同時轉換成物件
     return response.json();
   }).then(function (myJson) {
@@ -16,6 +17,13 @@ fetch("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorizati
             <div class="rain">降雨機率:${myJson.records.location[i].weatherElement[1].time[0].parameter.parameterName}</div>
             <div class="comfort">舒適度:${myJson.records.location[i].weatherElement[3].time[0].parameter.parameterName}</div>
         </div>`
+        if(myJson.records.location[i].weatherElement[1].time[0].parameter.parameterName>50){
+          picture.style.backgroundImage='url("./images/rain.png")'
+        } else if(myJson.records.location[i].weatherElement[1].time[0].parameter.parameterName<=50 &&myJson.records.location[i].weatherElement[1].time[0].parameter.parameterName>31){
+          picture.style.backgroundImage='url("./images/cloudy.png")'
+        } else{
+          picture.style.backgroundImage='url("./images/sun.png")'
+        }
     }
   });
 //取得天氣API資料
