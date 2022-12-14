be012441b3a32f219fc978dc0e80151090a19879
Corsilia <118946904+Corsilia@users.noreply.github.com>
2022-12-15 03:17:34 +0800

    天氣API

--- 06-王彩玉/天氣API/scripts/apicard.js
+++ 06-王彩玉/天氣API/scripts/apicard.js
@@ -1,6 +1,4 @@
 // 1. 圖片(下雨/晴天/陰天)2. 地區3. 最低/最高溫度4. 降雨機率5. 舒適度。圖片依照降雨機率51%以上 => 下雨50~31% => 陰天30%以下 => 晴天
-//預設是全部按鈕被點擊
-//用旗標做出預設按鈕被點擊的css狀態
 // https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-AA300EC1-31BA-465E-B669-6CA2C320A195 氣象資料
 const btn_all = document.querySelector('.btn-all');//全部
 const btn_n = document.querySelector('.btn-n');//北部
@@ -10,24 +8,29 @@ const btn_e = document.querySelector('.btn-e');//東部
 const btn_i = document.querySelector('.btn-i');//外島
 const time_first = document.querySelector('.time-first');
 const time_second = document.querySelector('.time-second');
-const time_third = document.querySelector('.time-third');
+const time_third = document.querySelector('.time-third');p
 const showcard = document.querySelector('.showcard');
 const timebox = document.querySelector('.time_box');
+const boxall = document.querySelector('main');
+const cover = document.querySelector('.cover');
 //以json縣市排名宣告陣列
-// var county = ['CHY','TPH','CYI','HSH','HSC','TPE','TNN','ILN','MAL','YLH','HWA','TXG','TTT','TYC','NTO','KHH','KMN','IUH','KLU','PEH','CWH','LNN'];
+var localend = [5, 1, 18, 13, 3, 4, 8, 11, 20, 14, 9, 0, 2, 6, 15, 17, 7, 10, 12, 19, 16, 21];//json排序地名，從北到南東部外島
 var county = ['TPE', 'TPH', 'KLU', 'TYC', 'HSH', 'HSC', 'MAL', 'TXG', 'CWH', 'NTO', 'YLH', 'CHY', 'CYI', 'TNN', 'KHH', 'IUH', 'ILN', 'HWA', 'TTT', 'PEH', 'KMN', 'LNN'];
 var countyselector = [];//縣市選擇器陣列
 var countydata = [];//縣市資料陣列
-var localend = [5, 1, 18, 13, 3, 4, 8, 11, 20, 14, 9, 0, 2, 6, 15, 17, 7, 10, 12, 19, 16, 21];//json排序地名，從北到南東部外島
-//用陣列設定縣市選擇器
-var place_name = [];
+var place_name = [];//用來放縣市名稱
+//三個時間陣列用來放不同時間抓出來各縣市的資料
 var first_time = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
 var second_time = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
 var third_time = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
-var cho = 0;
-var p = 0;
-let view_img = 'sundy';
-//依照0-1-2將整個物件存進去
+var cho = 0; //控制時間切換開關
+var p = 0; //控制地區切換開關
+var o = 0;
+let view_img = 'sundy'; //控制切換圖片
+btn_all.classList.add('hover');//預設「全部」按鈕為打開
+time_first.classList.add('hover');//預設第一個時間按鈕打開
+
+//依照time 0-1-2將整個物件存進去
 for (let x = 0; x < county.length; x++) {
     countyselector.push(document.querySelector('.' + county[x]));
     countyselector[x].classList.add("move");
@@ -40,23 +43,31 @@ fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorizati
 })//將回傳的資料下一步做處理，所以又.then去處理response.json
     .then(function (myJson) {
 
-        for (let x = 0; x < county.length; x++) {
+        // 按鈕顯示時間
+        let start_first = myJson.records.location[0].weatherElement[0].time[0].startTime;
+        let end_first = myJson.records.location[0].weatherElement[0].time[0].endTime;
+        time_first.innerHTML = start_first + '<br>｜<br>' + end_first;
+        let start_second = myJson.records.location[1].weatherElement[1].time[1].startTime;
+        let end_second = myJson.records.location[1].weatherElement[1].time[1].endTime;
+        time_second.innerHTML = start_second + '<br>｜<br>' + end_second;
+        let start_third = myJson.records.location[2].weatherElement[2].time[2].startTime;
+        let end_third = myJson.records.location[2].weatherElement[2].time[2].endTime;
+        time_third.innerHTML = start_third + '<br>｜<br>' + end_third;
 
+        //處理資料
+        for (let x = 0; x < county.length; x++) {
             let y = localend[x]; //排序過地名
 
-            // v選擇時間
+            // 選擇時間
             for (let z = 0; z < 5; z++) {
                 first_time[x].push(myJson.records.location[y].weatherElement[z].time[0]);
                 second_time[x].push(myJson.records.location[y].weatherElement[z].time[1]);
                 third_time[x].push(myJson.records.location[y].weatherElement[z].time[2]);
             }
-
             // 如果我的time為每個縣市的五個資料為前五個陣列資料(共110個)那可以設定變數CT++每五個歸零一次，同時R在每五個CT歸零時加一，這樣去做旗標，這樣的話就能分辨現在到哪
-            countydata.push(myJson.records.location[y]);//將資料以陣列順序輸入(這裡需要再檢查一次)
+            countydata.push(myJson.records.location[y]);//將資料以陣列順序輸入
             let locat = countydata[x].locationName;
-
             place_name.push(locat);
-
             let high_temp = countydata[x].weatherElement[4].time[2].parameter.parameterName;//第一時段最高溫
             let low_temp = countydata[x].weatherElement[2].time[2].parameter.parameterName;
             let weather_type = countydata[x].weatherElement[0].time[2].parameter.parameterName;
@@ -65,7 +76,7 @@ fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorizati
             // time[0]是當日晚上六點到隔日早上六點
             // time[1]是隔日早上六點到隔日晚上六點
             // time[2]是隔日晚上六點到後日早上六點
-            // console.log(high_temp);
+
             //選擇圖片
             if (rain_chance >= 51) {
                 view_img = 'rainy';
@@ -77,26 +88,40 @@ fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorizati
             //製作卡片
             showcard.innerHTML +=
                 `<div class="card card-${county[x]}">
-    <div class="img ${view_img}"></div>
-    <div class="local">${locat}</div>
-    <div class="temp">${high_temp}°C/${low_temp}°C</div>
-    <div class="winther">${weather_type}</div>
-    <div class="rain">降雨機率${rain_chance}%</div>
-    <div class="comfortable">${CI}</div>
-    </div>`
-            //因為一開始是預先設定time，是否要先將time時間抓出來，選擇時間的時候去做更動？ 例如陣列time 0 1 2
+                <div class="img ${view_img}"></div>
+                <div class="local">${locat}</div>
+                <div class="temp">高溫${high_temp}°C/低溫${low_temp}°C</div>
+                <div class="winther">${weather_type}</div>
+                <div class="rain">降雨機率${rain_chance}%</div>
+                <div class="comfortable">${CI}</div>
+                </div>`
         }
-        console.log(myJson);
+        // console.log(myJson);
+
+        //選到卡片的時候，卡片css變化
+        for (let x = 0; x < county.length; x++) {
+            let card = document.querySelectorAll('.card');
+            card[x].addEventListener('click', function () {
+                if (p == 0) {
+                    card[x].style.transform = 'scale(4)';
+                } else {
+                    card[x].style.transform = 'scale(2)';
+                }
+                card[x].style.zIndex = '1';
+                card[x].style.position = 'absolute';
+                cover.style.zIndex = '2';
+                document.querySelector('.' + county[x]).style.fill = 'orangered';
+                document.querySelector('.' + county[x]).style.transform = 'translateY(-3px)';
+            });
+        }
     });
 
-
-btn_all.classList.add('hover');//預設全部按鈕為打開
-time_first.classList.add('hover');//預設第一個時間按鈕打開
-function showtime(){
-    timebox.innerHTML ='現在時間：&nbsp;' + new Date();
-    setTimeout('showtime()',1000);
+//時間每秒更新一次放在onload裡面
+function showtime() {
+    timebox.innerHTML = '現在時間：&nbsp;' + new Date();
+    setTimeout('showtime()', 1000);
 }
-
+//點擊地區按鈕，將先全部隱藏
 function touch() {
     for (let x = 0; x < county.length; x++) {
         document.querySelector(".card-" + county[x]).style.display = 'none';
@@ -104,6 +129,8 @@ function touch() {
 }
 //新增按鈕CSS方法，原來是只給hover就好，不用給前面一堆為了權重等地屬性
 //記得在其他地方都要清除add的class
+
+//選擇地區按鈕
 function north() {
     btn_all.classList.remove('hover');
     btn_m.classList.remove('hover');
@@ -121,7 +148,6 @@ function north() {
     clear();
     p = 1;
 }
-
 function middle() {
     btn_all.classList.remove('hover');
     btn_n.classList.remove('hover');
@@ -182,7 +208,6 @@ function islands() {
     clear();
     p = 5;
 }
-
 function all() {
     btn_m.classList.remove('hover');
     btn_n.classList.remove('hover');
@@ -197,8 +222,8 @@ function all() {
     }
     clear();
     p = 0;
-    console.log(p);
 }
+//時間按鈕
 function firsttime() {
     time_second.classList.remove('hover');
     time_third.classList.remove('hover');
@@ -220,7 +245,7 @@ function thirdtime() {
     cho = 2;
     change_time(cho);
 }
-
+//變更時間變更卡片
 function change_time() {
     showcard.innerHTML = "";
     var type;
@@ -251,14 +276,31 @@ function change_time() {
             `<div class="card card-${county[x]}">
     <div class="img ${view_img}"></div>
     <div class="local">${place_name[x]}</div>
-    <div class="temp">${high_temp}°C/${low_temp}°C</div>
+    <div class="temp">高溫${high_temp}°C/低溫${low_temp}°C</div>
     <div class="winther">${weather_type}</div>
     <div class="rain">降雨機率${rain_chance}%</div>
     <div class="comfortable">${CI}</div>
     </div>`
     }
     all();
+    for (let x = 0; x < county.length; x++) {
+        let card = document.querySelectorAll('.card');
+        card[x].addEventListener('click', function () {
+            if (p == 0) {
+                card[x].style.transform = 'scale(4)';
+            } else {
+                card[x].style.transform = 'scale(2)';
+            }
+            card[x].style.zIndex = '1';
+            card[x].style.position = 'absolute';
+            cover.style.zIndex = '2';
+            document.querySelector('.' + county[x]).style.fill = 'orangered';
+            document.querySelector('.' + county[x]).style.transform = 'translateY(-3px)';
+        });
+    }
 }
+
+//讓卡片變換css能夠變大張
 function big() {
     for (let x = 0; x < county.length; x++) {
         let card = document.querySelectorAll('.card')
@@ -266,14 +308,17 @@ function big() {
     }
 }
 //清空點擊地圖特效，還要做點地圖會換頁
-function clear(){
+function clear() {
     for (let y = 0; y < county.length; y++) {
         document.querySelector('.' + county[y]).style.fill = 'rgba(1, 24, 19)';
+        document.querySelector('.' + county[y]).style.transform = 'translateY(0px)';
         document.querySelector('.card-' + county[y]).style.transform = 'scale(1)';
         document.querySelector('.card-' + county[y]).style.zIndex = '0';
         document.querySelector('.card-' + county[y]).style.position = 'relative';
+        cover.style.zIndex = '0';
     }
 }
+
 btn_all.addEventListener('click', all);
 btn_n.addEventListener('click', touch);
 btn_n.addEventListener('click', north);
@@ -288,6 +333,7 @@ btn_i.addEventListener('click', islands);
 time_first.addEventListener('click', firsttime);
 time_second.addEventListener('click', secondtime);
 time_third.addEventListener('click', thirdtime);
+boxall.addEventListener('dblclick', clear);
 
 // 設定地圖按鈕事件
 for (let x = 0; x < county.length; x++) {
@@ -296,56 +342,46 @@ for (let x = 0; x < county.length; x++) {
         //for將所有顏色先清空，再改固定一格的顏色，達到切換的效果
         let card = document.querySelector('.card-' + county[x]);
         for (let y = 0; y < county.length; y++) {
+            //初始化
             document.querySelector('.' + county[y]).style.fill = 'rgba(1, 24, 19)';
             document.querySelector('.card-' + county[y]).style.transform = 'scale(1)';
             document.querySelector('.card-' + county[y]).style.zIndex = '0';
             document.querySelector('.card-' + county[y]).style.position = 'relative';
+            document.querySelector('.card-' + county[y]).style.transform = 'translateY(0px)';
         }
-        if(x<6){
+        if (x < 6) {
             touch();
             north();
-        }else if(x<11){
+        } else if (x < 11) {
             touch();
             middle();
-        }else if(x<16){
+        } else if (x < 16) {
             touch();
             west();
-        }else if(x<19){
+        } else if (x < 19) {
             touch();
             east();
-        }else{
+        } else {
             touch();
             islands();
         }
-        
-        map.style.fill = 'yellow';
-        if(p == 0){
-            card.style.transform = 'scale(3, 3.5)';
-        }else{
-            card.style.transform = 'scale(1.5)';
-        }
-        card.style.zIndex = '1';
-        card.style.position ='absolute';
-        card.style.position ='absolute';
-        
+        map.style.fill = 'orangered';
+        map.style.transform = 'translateY(-3px)';
+        setTimeout(function () {
+            if (p != 0) {
+                card.style.transform = 'scale(2)';
+            }
+            card.style.zIndex = '1';
+            card.style.position = 'absolute';
+            card.style.position = 'absolute';
+            cover.style.zIndex = '2';
+        }, 50);
     }
+    //地圖點擊事件，放在迴圈裡讓所有地區都跑一次
     map.addEventListener('click', map_click);
 }
-
-
-
+//預設是全部按鈕被點擊
+//用旗標做出預設按鈕被點擊的css狀態
 // 定義在哪裡的問題，以及在那裡使用的問題，須查明是不是因為陣列是呼叫涵式之後才有作用，所以應該把function塞進去fetch裡面
 // document.querySelector(`'.card-${county[x]}'`);
-
-//將卡片改成用 querySelectorAll來選會不會比較好選？
-
-
 // 用旗標：選擇到第一個按鈕的時候，設定X=0;選到2 X=2 選到3 X=3
-
-
-///尚須修改時間條件式，有時間的話就用條件式去判斷
-
-
-//先做點到地圖的時候，那一塊地卡片能夠放大，有時間才來做卡片點擊，沒時間就只當成卡片了
-
-//卡片點擊只要直接放大就好，有時間才來把地圖也變色
